d29ec34b83a69ba797f254577dbc7a9c72245259
timofey stepanov <tima66112@gmail.com>
2022-02-28 15:02:49 +0300

    Create 1_лаб.pas

---
+++ 1_лаб.pas
@@ -0,0 +1,18 @@
+﻿program helloworld;
+var x:integer;
+var cos,sin:real;
+var n,B,p,g:real;
+
+begin
+  x:=1;
+  B:=0.7;
+  g:=sqrt(sqr(0.7)+0.1*B);
+  p:=1-(2.7)*sqr(B*g);
+  sin:=Arctan(x/Sqrt(1-sqr(x)));
+  cos:=Arctan(Sqrt(1-sqr(x))/x);
+  N:=(sin*B*P/cos*B*G)+(sqrt(p+g));
+  writeln(N)
+  
+  
+  
+end.
