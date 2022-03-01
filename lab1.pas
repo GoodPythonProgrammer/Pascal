2ea8fdc6a2b5b7302a86c6bd65b8bb5f42519f9f
timofey stepanov <tima66112@gmail.com>
2022-03-01 09:31:24 +0300

    lab1
    lab1

---
+++ lab1.pas
@@ -0,0 +1,10 @@
+﻿program lab1_23;
+var x:integer;
+    g,b,p,n:real;
+begin
+  b:=0.7;
+  g:=sqrt(sqr(b)+0.1*b);
+  p:=1-exp(-b*g);
+  n:=(arctan(b/sqrt(1-sqr(b))*p))/(arctan(sqrt(1-sqr(b))/b)*g)+ sqrt(p+g);
+  write('n(b)= ', n:0:5);
+end.    
